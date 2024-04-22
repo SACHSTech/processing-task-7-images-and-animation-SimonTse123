40a985109dba0806ee5d3808924d5f7fd3cfd469
SimonTse123 <simon.tse25@ycdsbk12.ca>
2024-04-22 10:05:55 -0400

    new file:   .vscode/settings.json 	new file:   Background.jpg 	new file:   Pegasus.png 	new file:   Peterpan.png 	modified:   Sketch.java

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,88 @@
 import processing.core.PApplet;
+import processing.core.PImage;
+
 
 public class Sketch extends PApplet {
-	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
-  public void settings() {
-	// put your size call here
-    size(400, 400);
-  }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
-  public void setup() {
-    background(210, 255, 173);
-  }
+  PImage imgBackground;
+  PImage imgPegasus;
+  PImage imgPeter;
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
-  public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+  float fltBackgroundX = 0;
+  float fltBackgroundY = 0;
 
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
+  float fltPegasusX = 0;
+  float fltPegasusY = 0;
+
+  float fltCircleX = width / 2;
+  float fltCircleY = height / 2;
+
+  float fltPeterX = 0;
+  float fltPeterY = 0;
+
+  float fltSpeedXP = 4;
+  float fltSpeedYP = 2;
+
+  float fltSpeedXC = 2;
+  float fltSpeedYC = 4;
+
+
+ 
+
+  float fltAngle = 0;
   
-  // define other methods down here.
+
+ 
+
+  public void settings() {
+    // put your size call here
+    size(512, 512);
+  }
+
+  public void setup() {
+    imgBackground = loadImage("Background.jpg");
+    imgBackground.resize(width, height);
+
+    imgPegasus = loadImage("Pegasus.png");
+    imgPegasus.resize(width / 10,height / 10);
+
+    imgPeter = loadImage("Peterpan.png");
+    imgPeter.resize(width / 8, height / 8);
+
+    background(imgBackground);
+  }
+
+
+  public void draw() {
+    background(imgBackground);
+
+    image(imgPegasus, fltPegasusX, fltPegasusY);
+    fltPegasusX += fltSpeedXP;
+    fltPegasusY += fltSpeedYP;
+
+    if(fltPegasusX < 0 || fltPegasusX > width - 50){
+      fltSpeedXP *= -1;
+    }
+    if(fltPegasusY < 0 || fltPegasusY > height - 50){
+      fltSpeedYP *= -1;
+    }
+    fill(245, 66, 138);
+    ellipse(fltCircleX, fltCircleY, 50, 50);
+    fltCircleX += fltSpeedXC;
+    fltCircleY += fltSpeedYC;
+
+    if(fltCircleX < 25 || fltCircleX > width - 25){
+      fltSpeedXC *= -1;
+    }
+    if(fltCircleY < 25 || fltCircleY > height - 25){
+      fltSpeedYC *= -1;
+    }
+    
+    image(imgPeter, fltPeterX, fltPeterY);
+    fltPeterX = cos(fltAngle) * 50 + 256;
+    fltPeterY = sin(fltAngle) * 50 + 200;
+    fltAngle -= 0.05f;
+
+
+  }
 }
